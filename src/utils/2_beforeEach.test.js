11c572f0378929e16761f09ccba4ad755223fc9a
Taeseok Ryu <yuteta@gmail.com>
2021-07-08 11:20:12 +0900

    테스트 전후 콜백 함수 정리

--- src/utils/2_beforeEach.test.js
+++ src/utils/2_beforeEach.test.js
@@ -16,10 +16,21 @@ let num = 0;
 // });
 
 // 하지만 beforeEach를 쓰면 다르다
-// beforeEach는 테스트 이전의 콜백
-// afterEach는 테스트 종료 후의 콜백
+// beforeEach는 각 테스트 이전의 콜백
+// afterEach는 각 테스트 종료 후의 콜백
 
+beforeAll(() => {
+  console.log("전체 test가 실행되기 전에");
+});
+afterAll(() => {
+  console.log("전체 test가 실행된 후에");
+});
 beforeEach(() => {
+  console.log("각 test가 실행되기 전에");
+  num = 0;
+});
+afterEach(() => {
+  console.log("각 test가 실행된 후에");
   num = 0;
 });
 
@@ -28,7 +39,6 @@ test("0 + 1 = 1", () => {
   expect(num).toBe(1);
 });
 
-// 이 시점에서 num이 1이기 때문에 오류 발생
 test("0 + 2 = 2", () => {
   num = add(num, 2);
   expect(num).toBe(2);
